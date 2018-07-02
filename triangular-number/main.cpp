1137e8899d06c6949a13f01a203a9aecfd1dd482
siddudubey <37614490+siddudubey@users.noreply.github.com>
2018-07-02 22:53:03 +0530

    added comments

--- triangular-number/main.cpp
+++ triangular-number/main.cpp
@@ -2,6 +2,12 @@
 #include <cmath>
 using namespace std;
 
+/**
+num = n*(n+1)/2
+=> 2*num + (1/2)^2 = n^2 + 2*n*(1/2) + (1/2)^2
+=> 2*num + (1/4) = (n + (1/2))^2
+=> sqrt(2*num + 0.25) - 0.5 = n
+*/
 bool isTriangular( int& n) {
     double sumcheck = (double) 2*n + 0.25;
     double nroot = sqrt(sumcheck) - 0.5;
